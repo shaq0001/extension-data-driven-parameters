43f92fbe2304c22db06952ec6d2f1761a0eb741e
Isaak shaqalle <shaq0001@gmail.com>
2021-05-25 19:18:14 +0000

    Updates

---
+++ precache-manifest.c030ba82a6e30c480a29bfefb2bab147.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1527dbc2a66c30344af6ef51cf8c4f96",
+    "url": "/extension-data-driven-parameters/index.html"
+  },
+  {
+    "revision": "f5cf954c2ae6391e9fc3",
+    "url": "/extension-data-driven-parameters/static/css/main.be502f0c.chunk.css"
+  },
+  {
+    "revision": "91ba66fbe99a8f0ef4ff",
+    "url": "/extension-data-driven-parameters/static/js/2.53265724.chunk.js"
+  },
+  {
+    "revision": "68c3cf5dec8f4a8b51507820f3df02c8",
+    "url": "/extension-data-driven-parameters/static/js/2.53265724.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5cf954c2ae6391e9fc3",
+    "url": "/extension-data-driven-parameters/static/js/main.fc977604.chunk.js"
+  },
+  {
+    "revision": "6643f296c46c69cc1e31",
+    "url": "/extension-data-driven-parameters/static/js/runtime-main.f521655e.js"
+  }
+]);
--- sandboxed/src/entry_config.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import Configure from './Configure';
-
-declare global {
-    interface Window { tableau: any; }
-}
-
-ReactDOM.render(<Configure />, document.getElementById('container'));
